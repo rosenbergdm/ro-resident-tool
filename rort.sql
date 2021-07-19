0ae5a14df282e0c96b7c88aee832d957f980e1a5
David Rosenberg <dmr@davidrosenberg.me>
2021-07-18 20:22:45 -0500

    starting schema work

---
+++ rort.sql
@@ -0,0 +1,20 @@
+CREATE TABLE hospital (
+  id SERIAL NOT NULL,
+  name VARCHAR(128)
+  -- Incomplete placeholder --
+);
+
+CREATE TABLE patient (
+  id SERIAL NOT NULL,
+  fname VARCHAR(64) NOT NULL,
+  lname VARCHAR(64) NOT NULL,
+  fullname VARCHAR(128) DEFAULT NULL, -- Only if the first / last thing doesnt work
+  dob DATE DEFAULT NULL,
+  mrn INTEGER NOT NULL,
+  hospital INTEGER NOT NULL REFERENCES hospitals.id
+
+
+
+
+);
+
